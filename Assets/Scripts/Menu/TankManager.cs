eac23f621a53dc220153ea485e5a13e25e3fb3b4
akuroujin <113434354+patakitamas2002@users.noreply.github.com>
2024-08-06 20:08:09 +0200

    Tank part selection

---
+++ Assets/Scripts/Menu/TankManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankManager : MonoBehaviour
+{
+    private Transform tank;
+    public HullDatabase hulls;
+    public TextMeshProUGUI hullName;
+
+    public TurretDatabase turrets;
+    public TextMeshProUGUI turretName;
+    public CannonDatabase cannons;
+    public TextMeshProUGUI cannonName;
+
+
+    private int hullIndex = 0;
+    private int turretIndex = 0;
+    private int cannonIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tank = this.transform;
+        UpdateTank();
+    }
+    public void NextHull()
+    {
+        hullIndex = hullIndex + 1 < hulls.hulls.Length ? hullIndex + 1 : 0;
+        UpdateTank();
+    }
+    public void NextTurret()
+    {
+        turretIndex = turretIndex + 1 < turrets.turrets.Length ? turretIndex + 1 : 0;
+        UpdateTank();
+    }
+    public void NextCannon()
+    {
+        cannonIndex = cannonIndex + 1 < cannons.cannons.Length ? cannonIndex + 1 : 0;
+        UpdateTank();
+    }
+    public void PreviousHull()
+    {
+        hullIndex = hullIndex - 1 < 0 ? hulls.hulls.Length : hullIndex - 1;
+        UpdateTank();
+    }
+    public void PreviousTurret()
+    {
+        turretIndex = turretIndex - 1 < 0 ? turrets.turrets.Length : turretIndex - 1;
+        UpdateTank();
+    }
+    public void PreviousCannon()
+    {
+        cannonIndex = cannonIndex - 1 < 0 ? cannons.cannons.Length : cannonIndex - 1;
+        UpdateTank();
+    }
+    private void UpdateTank()
+    {
+        foreach (Transform child in tank)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Hull hull = hulls.GetHull(hullIndex);
+        hullName.text = hull.Name;
+        GameObject hullModel = Instantiate(hull.Model, tank);
+        hull.Model.transform.SetParent(tank);
+
+        Turret turret = turrets.GetTurret(turretIndex);
+        turretName.text = turret.Name;
+        GameObject turretModel = Instantiate(turret.Model, hullModel.transform.GetChild(0));
+        turret.Model.transform.SetParent(tank);
+
+        Cannon cannon = cannons.GetPart(cannonIndex);
+        cannonName.text = cannon.Name;
+        GameObject cannonModel = Instantiate(cannon.Model, turretModel.transform.GetChild(0));
+        cannon.Model.transform.SetParent(tank);
+    }
+}
